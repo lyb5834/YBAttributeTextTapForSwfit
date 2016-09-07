32f683301eca53d74396fb54d4ed4051f468cca1
LYB <lyb5834@126.com>
2016-09-07 13:25:43 +0800

    Initial Commit

---
+++ YBAttributeTextTapForSwfit-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YBAttributeTextTapForSwfit-Demo
+//
+//  Created by LYB on 16/9/7.
+//  Copyright © 2016年 LYB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
